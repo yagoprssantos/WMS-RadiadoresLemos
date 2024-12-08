0da55d8592ff29e33de8dabc82e5e50e411744c7
yagoprssantos <yagprogramr@gmail.com>
2024-12-08 14:52:30 -0300

    Refatoração do StatusBar e melhorias na conexão
    - Adicionada nova coluna ao Grid no MainWindow.xaml para ícone de conexão
    - Removido botão de verificação de conexão do StatusBar
    - Adicionado ícone de conexão ao StatusBar com StackPanel
    - Alterada posição do item de data e hora no StatusBar
    - Adicionados usings no MainWindow.xaml.cs para System.IO e System.Text.Json
    - Removida atualização da barra de status e visibilidade do botão de verificação após tentativa de conexão
    - Atualizada mensagem de erro ao carregar dados para incluir falha ao carregar arquivos locais
    - Adicionada função UpdateConnectionStatus para atualizar status da conexão
    - Modificada função CarregarTodasTabelasNoCache para tentar carregar dados de arquivos locais se conexão com banco de dados falhar
    - Removidas funções que abriam abas específicas
    - Adicionada função LogoutButton_Click para lidar com logout e retornar à janela de login
    - Removida função VerifyConnectionButton_Click
    - Adicionada função ConnectionButton_Click para abrir aba de banco de dados ao clicar no ícone de conexão
    - Adicionada função ConnectionButton_MouseEnter para atualizar ToolTip do ícone de conexão com base no status da conexão

--- src/Views/MainWindow.xaml.cs
+++ src/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Text.Json;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
@@ -155,28 +157,22 @@ namespace WMS_RadiadoresLemos_WPF
                 // Inicializa os arquivos locais com dados do banco de dados, se ainda não existirem
                 DatabaseFileManager gerenciadorDeArquivos = new DatabaseFileManager();
                 await gerenciadorDeArquivos.InicializarArquivosAsync();
-
-                // Finaliza a conexão com o banco de dados
-                UpdateStatusBar("Conexão com o banco de dados estabelecida", Colors.DarkGreen);
-                VerifyConnectionButton.Visibility = Visibility.Collapsed;
             }
             catch (Exception ex)
             {
-                UpdateStatusBar("Erro ao conectar com o banco de dados", Colors.DarkRed);
-                MessageBox.Show($"Erro ao conectar com o banco de dados: {ex.Message}", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                UpdateStatusBar("Erro ao carregar dados", Colors.DarkRed);
+                MessageBox.Show($"Erro ao carregar dados, com banco de dados e com arquivos locais: {ex.Message}", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
 
                 // Adiciona alerta
                 AlertaCache.AdicionarAlerta("Erro",
                                             ex.Message.ToString(),
-                                            "Não foi possível conectar ao banco de dados. Possíveis motivos:\n" +
+                                            "Não foi possível carregar dados com sucesso. Possíveis motivos:\n" +
                                             "- Problemas de conexão com a internet;\n" +
                                             "- Configurações incorretas do banco de dados;\n" +
-                                            "- Serviço do banco de dados indisponível.",
+                                            "- Arquivos locais corrompidos ou ausentes.",
                                             "- Verifique sua conexão com a internet;\n" +
                                             "- Verifique as configurações do banco de dados;\n" +
-                                            "- Tente reconectar ou contate o suporte.");
-
-                VerifyConnectionButton.Visibility = Visibility.Visible;
+                                            "- Reinicie a aplicação");
             }
         }
 
@@ -221,48 +217,44 @@ namespace WMS_RadiadoresLemos_WPF
             timer.Start();
         }
 
+
         // Abas
-        // Função para abrir a aba de Registro de Entrada e Saída
         private void RegistroEntradaSaida_Click(object sender, RoutedEventArgs e)
         {
             ContentArea.Content = null;
             ContentArea.Content = new RegistroEntradaSaidaUserControl();
         }
 
-        // Função para abrir a aba de Controle de Estoque
         private void ControleEstoque_Click(object sender, RoutedEventArgs e)
         {
             ContentArea.Content = null;
             ContentArea.Content = new ControleEstoqueUserControl();
         }
 
-        // Função para abrir o Dashboard
         private void Dashboard_Click(object sender, RoutedEventArgs e)
         {
             ContentArea.Content = null;
             ContentArea.Content = new DashboardUserControl();
         }
 
-        // Função para abrir a aba de Banco de Dados
         private void BancoDados_Click(object sender, RoutedEventArgs e)
         {
             ContentArea.Content = null;
             ContentArea.Content = new BancoDadosUserControl();
         }
 
-        // Função para abrir a aba de Usuários
         private void Usuarios_Click(object sender, RoutedEventArgs e)
         {
             ContentArea.Content = null;
             ContentArea.Content = new UsuariosUserControl();
         }
 
-        // Função para abrir a aba de Notificações
         private void Notificacoes_Click(object sender, RoutedEventArgs e)
         {
             ContentArea.Content = null;
             ContentArea.Content = new NotificacoesUserControl();
         }
+
 
         // Botão de logout para retornar à janela de login
         private async void LogoutButton_Click(object sender, RoutedEventArgs e)
@@ -323,12 +315,6 @@ namespace WMS_RadiadoresLemos_WPF
             }
         }
 
-        // Verifica a conexão com o banco de dados e tenta reconectar
-        private void VerifyConnectionButton_Click(object sender, RoutedEventArgs e)
-        {
-            SetupDatabaseConnection();
-            SetupStatusBar();
-        }
 
         // Atualiza a barra de status com uma mensagem e uma cor
         private void UpdateStatusBar(string message, Color color)
@@ -337,80 +323,123 @@ namespace WMS_RadiadoresLemos_WPF
             StatusBar.Background = new SolidColorBrush(color);
         }
 
+        // Atualiza o status da conexão com o banco de dados
+        private void UpdateConnectionStatus(string status)
+        {
+            ConnectionStatus.Text = status;
+        }
+
+
         // Função para carregar todas as tabelas no cache
         private async Task CarregarTodasTabelasNoCache()
         {
             try
             {
-                var db = DatabaseConnect.Database ?? throw new InvalidOperationException("Conexão com o banco de dados não estabelecida.");
+                var db = DatabaseConnect.Database;
                 UpdateStatusBar("Carregando dados no cache...", Colors.DarkOrange);
 
                 // Lista de tabelas a serem carregadas no cache
                 var tabelas = new List<string>
-                    {
-                        "Produtos",
-                        "Usuarios",
-                        "Historico",
-                        "Movimentacoes"
-                    };
+        {
+            "Produtos",
+            "Usuarios",
+            "Historico",
+            "Movimentacoes"
+        };
+
+                var dbFileManager = new DatabaseFileManager();
 
                 // Para cada tabela, 
                 foreach (var tabela in tabelas)
                 {
-                    // Pega a referência da tabela
-                    var tabelaRef = db.Collection(tabela);
-                    // Pega o snapshot da tabela
-                    var snapshot = await tabelaRef.GetSnapshotAsync();
-
-                    // Lista de objetos para armazenar os dados da tabela
                     var listaObjetos = new List<object>();
-                    foreach (var doc in snapshot.Documents)
+
+                    // TODO: alterar de volta para != para conexão funcionar
+                    if (db != null)
                     {
-                        // Se a tabela for de produtos, converte o documento para ProdutoData e adiciona à lista
-                        if (tabela == "Produtos")
+                        // Pega a referência da tabela
+                        var tabelaRef = db.Collection(tabela);
+                        // Pega o snapshot da tabela
+                        var snapshot = await tabelaRef.GetSnapshotAsync();
+
+                        foreach (var doc in snapshot.Documents)
                         {
-                            var produto = doc.ConvertTo<ProdutoData>();
-                            listaObjetos.Add(produto);
+                            // Se a tabela for de produtos, converte o documento para ProdutoData e adiciona à lista
+                            if (tabela == "Produtos")
+                            {
+                                var produto = doc.ConvertTo<ProdutoData>();
+                                listaObjetos.Add(produto);
+                            }
+                            // Se a tabela for de usuários, converte o documento para UsuarioData e adiciona à lista
+                            else if (tabela == "Usuarios")
+                            {
+                                var usuario = doc.ConvertTo<UsuarioData>();
+                                listaObjetos.Add(usuario);
+                            }
+                            // Se a tabela for de histórico, converte o documento para LogData e adiciona à lista
+                            else if (tabela == "Historico")
+                            {
+                                var log = doc.ConvertTo<LogData>();
+                                listaObjetos.Add(log);
+                            }
+                            // Se a tabela for de movimentações, converte o documento para MovimentacaoData e adiciona à lista
+                            else if (tabela == "Movimentacoes")
+                            {
+                                var movimentacao = doc.ConvertTo<MovimentacaoData>();
+                                listaObjetos.Add(movimentacao);
+                            }
+                            else
+                            {
+                                // Se a tabela não for reconhecida, retorna uma exceção
+                                throw new InvalidOperationException($"Tabela '{tabela}' não reconhecida.");
+                            }
                         }
-                        // Se a tabela for de usuários, converte o documento para UsuarioData e adiciona à lista
-                        else if (tabela == "Usuarios")
+
+                        UpdateStatusBar("Dados carregados no cache - Banco de dados", Colors.DarkGreen);
+                        UpdateConnectionStatus("Conectado");
+                    }
+                    else
+                    {
+                        // Se não houver conexão com o banco de dados, carrega os dados dos arquivos locais
+                        string caminhoArquivo = tabela switch
                         {
-                            var usuario = doc.ConvertTo<UsuarioData>();
-                            listaObjetos.Add(usuario);
-                        }
-                        // Se a tabela for de histórico, converte o documento para LogData e adiciona à lista
-                        else if (tabela == "Historico")
+                            "Produtos" => dbFileManager.CaminhoArquivoProdutos,
+                            "Usuarios" => dbFileManager.CaminhoArquivoUsuarios,
+                            "Historico" => dbFileManager.CaminhoArquivoLogs,
+                            "Movimentacoes" => dbFileManager.CaminhoArquivoMovimentacoes,
+                            _ => throw new InvalidOperationException($"Tabela '{tabela}' não reconhecida.")
+                        };
+
+                        if (File.Exists(caminhoArquivo))
                         {
-                            var log = doc.ConvertTo<LogData>();
-                            listaObjetos.Add(log);
+                            var json = await File.ReadAllTextAsync(caminhoArquivo);
+                            var objetos = tabela switch
+                            {
+                                "Produtos" => JsonSerializer.Deserialize<List<ProdutoData>>(json)?.Cast<object>().ToList(),
+                                "Usuarios" => JsonSerializer.Deserialize<List<UsuarioData>>(json)?.Cast<object>().ToList(),
+                                "Historico" => JsonSerializer.Deserialize<List<LogData>>(json)?.Cast<object>().ToList(),
+                                "Movimentacoes" => JsonSerializer.Deserialize<List<MovimentacaoData>>(json)?.Cast<object>().ToList(),
+                                _ => throw new InvalidOperationException($"Tabela '{tabela}' não reconhecida.")
+                            };
+
+                            if (objetos != null)
+                            {
+                                listaObjetos.AddRange(objetos);
+                            }
                         }
-                        // Se a tabela for de movimentações, converte o documento para MovimentacaoData e adiciona à lista
-                        else if (tabela == "Movimentacoes")
-                        {
-                            var movimentacao = doc.ConvertTo<MovimentacaoData>();
-                            listaObjetos.Add(movimentacao);
-                        }
-                        else
-                        {
-                            // Se a tabela não for reconhecida, retorna uma exceção
-                            throw new InvalidOperationException($"Tabela '{tabela}' não reconhecida.");
-                        }
+
+                        UpdateStatusBar("Dados carregados do arquivo - Usando arquivos locais", Colors.Purple);
+                        UpdateConnectionStatus("Desconectado");
                     }
 
                     // Adiciona a lista de objetos ao cache
                     DadosCache.Tabelas[tabela] = listaObjetos;
                 }
-
-                // Todo esse código resulta em um cache com todas as tabelas carregadas
-                // O cache é um dicionário onde a chave é o nome da tabela e o valor é uma lista de objetos,
-                // onde cada objeto é um documento da tabela. Isso permite um acesso rápido e offline aos dados.
-
-                UpdateStatusBar("Dados carregados no cache - Pronto para uso", Colors.DarkGreen);
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Erro ao carregar as tabelas no cache: {ex.Message}", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
-                UpdateStatusBar("Erro ao carregar dados no cache - Verifique a conexão com o banco de dados", Colors.DarkRed);
+                UpdateStatusBar("Erro ao carregar dados no cache", Colors.DarkRed);
 
                 // Adiciona alerta
                 AlertaCache.AdicionarAlerta("Erro",
@@ -422,10 +451,9 @@ namespace WMS_RadiadoresLemos_WPF
                                             "- Verifique sua conexão com a internet;\n" +
                                             "- Verifique as configurações do banco de dados;\n" +
                                             "- Tente reconectar ou contate o suporte.");
-
-                VerifyConnectionButton.Visibility = Visibility.Visible;
             }
         }
+
 
         // Função que representa a animação de notificação de alerta
         private void OnAlertaAdicionado(AlertaData alerta)
@@ -478,5 +506,25 @@ namespace WMS_RadiadoresLemos_WPF
             NotificationToolTip.Content = $"Você tem {_notificationCount} novas notificações";
         }
 
+
+        // Função para abrir conexão com banco de dados
+        private void ConnectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Abre a aba de banco de dados
+            ContentArea.Content = null;
+            ContentArea.Content = new BancoDadosUserControl();
+        }
+        private void ConnectionButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            // Atualizar o ToolTip com base na conexão com o banco de dados
+            if (DatabaseConnect.Database != null)
+            {
+                ConnectionToolTip.Content = "Conectado ao banco de dados - usando dados online";
+            }
+            else
+            {
+                ConnectionToolTip.Content = "Desconectado do banco de dados - usando arquivos locais offline. Reconecte para sincronizar informações";
+            }
+        }
     }
 }
